cb54ba3e07bcaef008b869be7aac46f9ff223dc4
Shirley Fang <fangyixuan0123@gmail.com>
2024-05-14 23:47:14 -0400

    Used hooks and props for event handling

--- part1-React/Practice/src/App.jsx
+++ part1-React/Practice/src/App.jsx
@@ -1,31 +1,72 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-        <Display counter={counter} />
-        <Button onClick={increaseByOne} text='plus' />
-        <Button onClick={setToZero} text='zero' />
-        <Button onClick={decreaseByOne} text='minus' />
+      button press history : {props.allClicks.join('')}
     </div>
+  )
+}
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+  
+  return (
+    <div>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text={left} />
+        <Button handleClick={handleRightClick} text={right} />
+        {right}
+        <History allClicks={allClicks} />
+        <p>total {total}</p>
+      </div>
+      <div>
+      {value}
+      <Button handleClick={() => setToValue(1000)} text='thousand'/>
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      </div>
+    </div>
   )
 }
 
