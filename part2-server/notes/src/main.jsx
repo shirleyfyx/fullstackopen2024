233fcaa894c0c7cc173bb20e9ba1510d0c208db9
Shirley Fang <fangyixuan0123@gmail.com>
2024-06-22 23:59:35 -0400

    Use axios to communicate with the server

---
+++ part2-server/notes/src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client'
+import axios from 'axios'
+import App from './App'
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
+
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     console.log(notes)
+// })
+
+// const promise = axios.get('http://localhost:3001/notes')
+// promise.then(response => {
+//   console.log(response)
+// })
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
